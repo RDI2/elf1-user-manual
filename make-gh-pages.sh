e7bdcd98410489ba8685edacfa7f9ce54739ac6b
Koji Tanaka <kj.tanaka@gmail.com>
2016-01-03 20:47:59 -0500

    setup make-gh-pages

--- make-gh-pages.sh
+++ make-gh-pages.sh
@@ -5,8 +5,11 @@ DIR=$(pwd)
 gitbook build
 
 cd /tmp
-git clone git@github.com:RDI2/elf1-user-manual.git
+git clone -b gh-pages git@github.com:RDI2/elf1-user-manual.git
 cd elf1-user-manual
-git checkout gh-pages
 rsync -a $DIR/_book/ .
-
+git add .
+git commit -am "update gh-pages"
+git push
+cd ..
+rm -rf elf1-user-manual
